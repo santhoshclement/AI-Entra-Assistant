2edc047cb5b556c067437ddcbe8690c3a16c6c9d
santhoshclement <53114856+santhoshclement@users.noreply.github.com>
2026-06-01 10:28:05 +0400

    Update auth.js

--- templates/js/auth.js
+++ templates/js/auth.js
@@ -1,1 +1,10 @@
+const msalInstance = new msal.PublicClientApplication(msalConfig);
 
+async function signIn() {
+    try {
+        const loginResponse = await msalInstance.loginPopup(loginRequest);
+        console.log("User Logged In", loginResponse.account);
+    } catch (error) {
+        console.error(error);
+    }
+}
